ae74c100a8faf7378222b359d32af13cd6209c27
HumayNajafova <humaynecefovs@gmail.com>
2023-08-12 09:29:40 +0400

    first commit

---
+++ BOM/src/script.js
@@ -0,0 +1,23 @@
+const ball = document.querySelector(".ball");
+const ballImg = document.querySelector(".ball-img");
+const person = document.querySelector('.person')
+const section = document.querySelector("section");
+const music = document.getElementById('play')
+
+// window.addEventListener('load', () =>{
+//   music.play()
+//   console.log('test');
+// })
+  ball.addEventListener("click", () => {
+
+    ball.classList.toggle('active')
+    if (ball.classList.contains('active')) {
+      ballImg.style.marginLeft=`${section.clientWidth / 2 - ballImg.clientWidth/2-person.clientWidth}px`
+      ballImg.style.marginTop=`${section.clientHeight / 2 - ballImg.clientHeight/2}px`
+    }
+      
+    
+  });
+
+
+
